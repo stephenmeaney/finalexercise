a18fde18040150432ca6a222a8619f247218abc9
Stephen Meaney <smeaney@solstice.com>
2018-09-11 08:22:28 -0500

    Adding modules and dependencies

---
+++ account-address-services/src/main/java/com/stephenmeaney/services/AccountAddressServicesApplication.java
@@ -0,0 +1,12 @@
+package com.stephenmeaney.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAddressServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountAddressServicesApplication.class, args);
+	}
+}
